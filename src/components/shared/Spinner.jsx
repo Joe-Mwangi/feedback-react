d2ac73f4c1b7c869f3d6f1f25c588691a0d8bbec
Joe-Mwangi <joem99271@gmail.com>
2023-02-20 07:29:34 +0300

    centered gif

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -1,9 +1,11 @@
-import Loading from '../../assets/loading.gif'
+import loading from '../../assets/loading.gif'
 function Spinner() {
   const styles = {
     height: '3rem',
-    width: '3rem'
+    width: '3rem',
+    margin: 'auto',
+    display: 'block'
   }
-  return  <img src={Loading} alt="Loading..." style={styles} />
+  return  <img src={loading} alt="Loading..." style={styles} />
 }
 export default Spinner
